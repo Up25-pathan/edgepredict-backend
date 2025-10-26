a4c171698022e39c9c91d5e1030ab5706f256830
Umar Salim Pathan <78787284+Up25-pathan@users.noreply.github.com>
2025-10-26 11:39:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    salt = Column(String) # <-- NEW: Add column to store the salt
     is_active = Column(Boolean, default=True)
     simulations = relationship("Simulation", back_populates="owner")
     materials = relationship("Material", back_populates="owner")
